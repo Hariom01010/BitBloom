44d1cd86caf183ada04f4f06ce556d8353ea7eaa
Hariom Vashista <rishivashista4@gmail.com>
2024-12-21 17:19:38 +0530

    frontend for bitbloom bot to invite it to your server

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import bg from "./assets/BitBloom.png"
+
+function App() {
+
+  return (
+    <>
+      <h1>BitBloom</h1>
+      <h2>Your Personal Discord Query Manager</h2>
+
+      <button>
+        <a href="https://discord.com/oauth2/authorize?client_id=1319351993541853249" target="_blank">Invite Bot</a>
+      </button>
+    </>
+  )
+}
+
+export default App
